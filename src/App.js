2dbe1ba6e15f602e29810f2223767d43eaa6773b
sam <92265654+simonem-f@users.noreply.github.com>
2023-04-02 20:46:23 +0200

    add age calculation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
 import './App.css';
 import AgeForm from './components/AgeForm.js'
 import AgeCalculation from './components/AgeCalculation.js'
-
-function caluclateAge(date) {
-  console.log(date)
-}
+import React, { useState } from 'react';
 
 function App() {
+  const [age, setAge] = useState({
+    days: null,
+    months: null,
+    years: null,
+  })
+
+  function mapMonthDays(month, year) {
+    if (month === 2) {
+      return year % 4 === 0 ? 29 : 28;
+    }
+    if([4, 6, 9, 11].includes(month)) {
+      return 30;
+    }
+    return 31;
+  }
+
+  function caluclateAge(date) {
+    const now = new Date();
+
+    let yearsCount = 0;
+    let monthsCount = 0;
+    let daysCount = 0;
+
+    let repDay = 0;
+    let repMonth = 0;
+
+    if(now.getDate() >= date.getDate()) {
+      daysCount = now.getDate() - date.getDate();
+    } else {
+      daysCount = mapMonthDays(date.getMonth(), date.getFullYear()) - date.getDate() + now.getDate();
+      repDay = 1;
+    }
+
+    if(now.getMonth() >= date.getMonth()) {
+      monthsCount = now.getMonth() - date.getMonth();
+    } else {
+      monthsCount = 12 - date.getMonth() + now.getMonth() - repDay;
+      repMonth = 1;
+    }
+
+    yearsCount = now.getFullYear() - date.getFullYear() - repMonth;
+
+    setAge({
+      ...age,
+      days: daysCount,
+      months: monthsCount,
+      years: yearsCount,
+    })
+  }
+
   return (
     <div className="App">
       <AgeForm onSubmitButton={caluclateAge} />
-      <AgeCalculation days={4}/>
+      <AgeCalculation days={age.days} months={age.months} years={age.years}/>
     </div>
   );
 }
--- src/components/AgeCalculation.js
+++ src/components/AgeCalculation.js
@@ -3,9 +3,9 @@ import './AgeCalculation.css';
 function AgeCalculation(props) {
   return (
     <div className="AgeCalculation">
-			<p><span className='number'>{props.years ? props.years : '--'}</span> years</p>
-			<p><span className='number'>{props.months ? props.months : '--'}</span> months</p>
-			<p><span className='number'>{props.days ? props.days : '--'}</span> days</p>
+			<p><span className='number'>{typeof props.years === 'number' ? props.years : '--'}</span> years</p>
+			<p><span className='number'>{typeof props.months === 'number' ? props.months : '--'}</span> months</p>
+			<p><span className='number'>{typeof props.days === 'number' ? props.days : '--'}</span> days</p>
     </div>
   );
 }
